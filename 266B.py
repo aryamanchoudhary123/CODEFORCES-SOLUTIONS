68d75292b5ff05a9411106a7944dfe7cd6797dcf
aryamanchoudhary123 <aryaman.choudhary01@gmail.com>
2020-06-30 22:47:55 +0530

    Update 266B.py

--- 266B.py
+++ 266B.py
@@ -1,12 +1,14 @@
 n,t=map(int,input().split())
 s=list(input())
 c=0
-for i in range(n-1):
-    if s[i]=="B":
-        for j in range(i+1,n-1):
-            if c < t:
-                if s[j]=="G" :
-                    s[i],s[j]=s[j],s[i]
-                    c+=1 
-
+while t>0 :
+    t-=1
+    i=0
+    while i < n-1:
+        if s[i]=="B" and s[i+1]=="G":
+            s[i]="G"
+            s[i+1]="B"
+            i+=1
+        i+=1    
+        
 print(''.join(s))
